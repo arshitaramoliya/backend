ce7ab471bff1ace0f28a19095fa2ffbc66eae523
Arshita Ramoliya <arshitaramoliya.work@gmail.com>
2025-04-08 10:02:00 +0530

    Add star pyramid pattern program

--- Test1.java
+++ Test1.java
@@ -8,3 +8,14 @@ public class Test1{
         }
     }
 }
+// Added: Star pyramid pattern
+class StarPyramid {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j >= i; j--) System.out.print(" ");
+            for (int k = 1; k <= (2 * i - 1); k++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
